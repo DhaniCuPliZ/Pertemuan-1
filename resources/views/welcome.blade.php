f7e795da24a49b0062013cd61a4907d1b68f830e
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2026-02-27 16:30:32 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <style>
+            body {
+                font-family: 'Instrument Sans', sans-serif;
+            }
+        </style>
+    @endif
+</head>
+<body class="bg-[#FDFDFC] text-[#1b1b18] min-h-screen flex items-center justify-center">
+
+    <div class="bg-white shadow-lg rounded-lg p-10 text-center">
+        <h1 class="text-3xl font-bold mb-4">
+            Rafli Ramadhani Oktavianto Khufi
+        </h1>
+        <p class="text-xl mb-6">
+            20230140127
+        </p>
+        <button class="bg-[#FDFDFC] border border-gray-300 rounded px-4 py-2 hover:bg-gray-100">
+            Modul Pertemuan 1
+        </button>
+    </div>
+
+</body>
+</html>
