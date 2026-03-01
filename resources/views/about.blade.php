17f4b3d94e740ad26d94c44cdd8460fa9e5331b6
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2026-03-01 21:25:18 +0700

    Tugas Pertemuan 2

---
+++ resources/views/about.blade.php
@@ -0,0 +1,34 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            About
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white p-6 rounded shadow">
+                <h3 class="text-lg font-bold mb-4">Biodata Mahasiswa</h3>
+
+                <table class="table-auto">
+                    <tr>
+                        <td class="pr-4 font-semibold">Nama</td>
+                        <td>: Rafli Ramadhani Oktavianto Khufi</td>
+                    </tr>
+                    <tr>
+                        <td class="pr-4 font-semibold">NIM</td>
+                        <td>: 20230140127</td>
+                    </tr>
+                    <tr>
+                        <td class="pr-4 font-semibold">Program Studi</td>
+                        <td>: Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td class="pr-4 font-semibold">Hobi</td>
+                        <td>: Futsal, Hiking</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
