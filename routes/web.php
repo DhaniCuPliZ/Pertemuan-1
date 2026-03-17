35a3f4d8147a71856939aa80bf7f34617c4b00e0
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2026-03-17 20:31:44 +0700

    menambahkan file create,edit,index,view blade.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController; 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AboutController;
 
@@ -15,6 +16,10 @@ Route::get('/dashboard', function () {
 Route::get('/about', [AboutController::class, 'index'])
     ->middleware(['auth'])
     ->name('about');
+
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
 Route::middleware('auth')->group(function () {
     Route::get('/product', [ProductController::class, 'index'])->name('product.index');
