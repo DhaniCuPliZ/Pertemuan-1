d809952f10c4b2ebb9200c0854f1b513e007ba2a
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2026-04-24 20:00:45 +0700

    Membuat Blade Component Add, Edit, Delete button

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -4,6 +4,7 @@
             <div class="bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-4">
 
+                    
                     {{-- Header --}}
                     <div class="flex items-center justify-between mb-6">
                         <div>
@@ -12,13 +13,7 @@
                         </div>
 
                         @can('manage-product')
-                            <a href="{{ route('product.create') }}"
-                               class="inline-flex items-center gap-2 px-4 py-2 bg-indigo-600 hover:bg-indigo-500 text-white text-sm font-semibold rounded-lg transition duration-150 shadow-sm">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
-                                </svg>
-                                Add Product
-                            </a>
+                            <x-add-product :url="route('product.create')" name="Product" />
                         @endcan
                     </div>
 
